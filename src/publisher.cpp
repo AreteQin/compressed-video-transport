63535beadc0feaa8d68fdb39f55fa107be08c6cd
Qiaomeng Qin <qinqiaomeng@outlook.com>
2022-07-12 15:10:35 -0400

    created

---
+++ src/publisher.cpp
@@ -0,0 +1,34 @@
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <ros/ros.h>
+#include <sensor_msgs/image_encodings.h>
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "pub_cam_node");
+    ros::NodeHandle nh;
+
+    cv::VideoCapture capture(0); //read the video from camera
+    if (!capture.isOpened()) {
+        ROS_ERROR_STREAM("Failed to open video device\n");
+        ros::shutdown();
+    }
+
+    //image_transport will publish the video that can be compressed
+    image_transport::ImageTransport it(nh);
+    image_transport::Publisher pub_color = it.advertise("/color", 1);
+
+    cv::Mat image, depth;
+
+    while (ros::ok()) {
+        capture >> image; //load
+        if (image.empty()) {
+            ROS_ERROR_STREAM("Failed to capture image!");
+            ros::shutdown();
+        }
+        pub_color.publish(cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg());
+    }
+
+    ros::spin();
+}
---
+++ src/receiver.cpp
@@ -0,0 +1,48 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+//#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+class ImageReceiver {
+private:
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    image_transport::Subscriber image_sub_;
+    std::string OPENCV_WINDOW;
+public:
+    ImageReceiver(const std::string &topic_name)
+            : it_(nh_) {
+        OPENCV_WINDOW = topic_name;
+        // Subscrive to input video feed and publish output video feed
+        image_sub_ = it_.subscribe(OPENCV_WINDOW, 1,
+                                   &ImageReceiver::imageCb, this);
+        cv::namedWindow(OPENCV_WINDOW);
+    }
+
+    ~ImageReceiver() {
+        cv::destroyWindow(OPENCV_WINDOW);
+    }
+
+    void imageCb(const sensor_msgs::ImageConstPtr &msg) {
+        cv_bridge::CvImagePtr cv_ptr;
+        try {
+            cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
+        }
+        catch (cv_bridge::Exception &e) {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+            return;
+        }
+        cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+        cv::waitKey(1);
+    }
+};
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "rgbd_receiver");
+    ImageReceiver color("/color");
+    ros::spin();
+    return 0;
+}
